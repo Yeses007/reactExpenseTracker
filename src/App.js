798c135f0d17957f2d461fa6996d0d39e7f5db2b
jrshaktiman <notyourmail04@gmail.com>
2021-09-21 17:13:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Temp from "./temp";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+      <Temp></Temp>
+
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/temp.js
@@ -0,0 +1,9 @@
+function temp(){
+    return(
+        <div>
+<h1>My new Code...</h1>
+        </div>
+    );
+}
+
+export default temp;
